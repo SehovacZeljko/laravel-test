33d445796eb702b3ef2575e3c1a1795198dfb4dc
Zeljko_Sehovac <zeljko.sehovac@singularity.is>
2025-08-22 11:07:01 +0200

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NinjaController;
+use App\Http\Controllers\AuthController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// auth
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('guest')->controller(AuthController::class)->group(function () {
+    Route::get('/register', 'showRegister')->name('show.register');
+    Route::get('/login', 'showLogin')->name('show.login');
+    Route::post('/register', 'register')->name('register');
+    Route::post('/login', 'login')->name('login');
+});
+
+// ninjas
+Route::middleware('auth')->controller(NinjaController::class)->group(function () {
+    Route::get('/ninjas', 'index')->name('ninjas.index');
+    Route::get('/ninjas/create', 'create')->name('ninjas.create');
+    Route::get('/ninjas/{ninja}', 'show')->name('ninjas.show');
+    Route::post('/ninjas', 'store')->name('ninjas.store');
+    Route::delete('/ninjas/{ninja}', 'destroy')->name('ninjas.destroy');
+});
